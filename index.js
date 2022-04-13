48745ec10c570e57bac182be2e76fe2764a2a2f8
Verity Carlos <verity.carlos@gmail.com>
2022-04-13 16:52:01 -0500

    commit tree 0f2ada336e785d9739cf0aa1e6ac319ad8666f9a

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let multiply, num1, num2;
+num1=2;
+num2=31;
+multiply = num1 * num2;
 
-// Write your code here
+
+let random;
+random = Math.round(Math.random()+1);
+
+
+let mod, num3, num4;
+num3=10;
+num4=6;
+mod = (num3%num4);
+
+
+let max;
+max = Math.max(1,10,20);
